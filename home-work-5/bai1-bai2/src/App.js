70f310767b60b8919385c0ddf9188cd64ce3a15c
ngiad123 <follownghia002@gmail.com>
2022-07-01 23:29:30 +0700

    bai 1 bai 2 api scroll

--- home-work-5/bai1-bai2/src/App.js
+++ home-work-5/bai1-bai2/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import Bai1 from './Compunents/Bai1';
 
 
 function App() {
@@ -14,13 +15,14 @@ function App() {
   }
 
   return (
-    <div>
+    <>
+      <Bai1 />
       <h1>Bài 2</h1>
       <button onClick={fatchData}>Call api</button>
       {dataApi ? dataApi.map((item, i) => {
         return <h2 key={i}>{item.title}</h2>
       }) : <h2>Loading?</h2>}
-    </div>
+    </>
   );
 }
 
