8a00aabd5ff7ac969f2e32c1b1ad45517cbc49ba
ngiad123 <follownghia002@gmail.com>
2022-07-02 10:59:18 +0700

    call-api-useEff

--- home-work-5/bai1-bai2/src/App.js
+++ home-work-5/bai1-bai2/src/App.js
@@ -5,20 +5,32 @@ import Bai1 from './Compunents/Bai1';
 
 function App() {
   const [dataApi, setDataApi] = useState(null)
+  const [checkApi, setCheckApi] = useState(true)
   useEffect(() => {
-    document.title = "call api"
-  })
-  const fatchData = async () => {
-    const res = await fetch(`https://fakestoreapi.com/products`)
-    const data = await res.json()
-    setDataApi(data)
-  }
+    document.title = `call api ${checkApi}`
+    const fatchData = async () => {
+      const res = await fetch(`https://fakestoreapi.com/products`)
+      const data = await res.json()
+      setDataApi(data)
+    }
+    fatchData()
+    return () => {
+      setDataApi(null)
+    }
+  }, [checkApi])
+  // const fatchData = async () => {
+  //   const res = await fetch(`https://fakestoreapi.com/products`)
+  //   const data = await res.json()
+  //   setDataApi(data)
+  // }
 
   return (
     <>
       <Bai1 />
       <h1>Bài 2</h1>
-      <button onClick={fatchData}>Call api</button>
+      <button onClick={() => {
+        setCheckApi(!checkApi)
+      }}>Call api</button>
       {dataApi ? dataApi.map((item, i) => {
         return <h2 key={i}>{item.title}</h2>
       }) : <h2>Loading?</h2>}
